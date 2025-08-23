862b1a13895690e02333738db56ead925e3367bb
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2025-08-23 15:58:18 +0600

    added js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+ 
+
+const exploreBtn = document.getElementById("explore-btn")
+
+console.log(exploreBtn)
+
+exploreBtn.addEventListener("click", function() {
+    console.log("Explore Done")
+})
+
+
+// const cardImg = document.getElementById("card-img")
+
+// console.log(cardImg.getAttribute("src"))
+// cardImg.setAttribute("src", "http://i.ibb.co.com/p0w744T/pet-1/jpg")
